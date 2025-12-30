a1ee7f8f7c800618953f3b3baa2a8456336d61ee
tarbipyakurel21 <tarbipyakurel21@gmail.com>
2025-12-30 08:54:06 -0600

    a little change

---
+++ backend/src/test/java/com/quiethelp/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quiethelp.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
